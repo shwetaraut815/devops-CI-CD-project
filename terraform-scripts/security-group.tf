4e9b9c7fc9e3f074e05dfc3de41d4ff84640f7c1
shwetaraut815 <shwetaraut815@gmail.com>
2025-11-03 16:20:08 +0000

    ci-cd project

---
+++ terraform-scripts/security-group.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "project_sg" {
+
+      name        = "project-sg"
+      vpc_id      = aws_vpc.project-vpc.id
+
+
+ dynamic "ingress" {
+    for_each = [22, 80, 8080]  # List of ports
+    content {
+      description = "Allow port ${ingress.value}"
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+ egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
